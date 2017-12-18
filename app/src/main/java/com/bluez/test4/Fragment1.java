9193e9c3a1621f4e79f3774cc1dbe1740b101dfb
Swaraj Hota <swarajhota353@gmail.com>
2017-12-19 01:52:42 +0530

    Initial Commit.

---
+++ app/src/main/java/com/bluez/test4/Fragment1.java
@@ -0,0 +1,27 @@
+package com.bluez.test4;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bluez on 19/12/17.
+ */
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Asimov's 1st Law");
+    }
+}
